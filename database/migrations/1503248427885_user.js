c9959be654e6f6c193b1ad8532fe6d83476feb1b
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-10-29 13:42:59 -0300

    chore: Remover e editar arquivos do template padrão.

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -7,10 +7,11 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
+      table.timestamps()
+
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
-      table.timestamps()
     })
   }
 
